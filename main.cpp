66d7ac69f49caa5f58533d9127c9ed2ebb99f09d
Gabriel J <GLJimenez516@gmail.com>
2024-10-04 14:50:52 -0400

    Update main.cpp
    Fixed code by adding a temporary variable, adding [] after argv, and using #include <iterator>

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include <list>
 #include <iostream>
+#include <iterator>
 
-int main(int argc, char* argv) {
+int main(int argc, char* argv[]) {
     std::list<int> intList;
     for (int i = 1; i < argc; i++) {
-        intList.push_back(argv[i]);
+        int temp = std::atoi(argv[i]);
+        intList.push_back(temp);
     }
 
     for (std::list<int>::reverse_iterator itr = intList.rbegin(); itr != intList.rend(); itr++) {
